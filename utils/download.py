a4495329362d1299b6f1008ab858dd9230b6a659
Lydia <39285530+qiaor1@users.noreply.github.com>
2020-11-05 17:15:42 -0800

    Update download.py

--- utils/download.py
+++ utils/download.py
@@ -7,9 +7,14 @@ from utils.response import Response
 
 def download(url, config, logger=None):
     host, port = config.cache_server
-    resp = requests.get(
-        f"http://{host}:{port}/",
-        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
+    while True:
+        try:
+            resp = requests.get(
+                f"http://{host}:{port}/",
+                params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
+            break
+        except:
+            continue
     if resp:
         return Response(cbor.loads(resp.content))
     logger.error(f"Spacetime Response error {resp} with url {url}.")
